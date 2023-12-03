90d44cf528c665d73a1a40a10c2f5870c5a9cfc3
The-Graze <reece2004f@gmail.com>
2023-12-03 08:19:24 +0000

    a

--- MenuMute/PluginInfo.cs
+++ MenuMute/PluginInfo.cs
@@ -5,7 +5,7 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.graze.gorillatag.menumute";
+        public const string GUID = "com.graze.lethalcompany.menumute";
         public const string Name = "MenuMute";
         public const string Version = "1.0.0";
     }
