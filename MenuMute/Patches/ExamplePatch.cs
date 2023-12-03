a696d8545ea302d4fd511ac193116c4da94c962d
The-Graze <reece2004f@gmail.com>
2023-12-03 08:09:16 +0000

    a

---
+++ MenuMute/Patches/ExamplePatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace MenuMute.Patches
+{
+    [HarmonyPatch(typeof(MenuManager))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    internal class ExamplePatch
+    {
+        private static void Postfix(MenuManager __instance)
+        {
+            if (SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                __instance.gameObject.GetComponent<AudioSource>().clip = null;
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ MenuMute/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuMute
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        Plugin()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+    }
+}
